31fbb3b0fcb4a6f56df2fd95323aeed7217e3330
Trương Minh Trọng <trong.truong89@gmail.com>
2022-01-02 10:51:42 +0700

    fix open-mobileshop

--- quantri/gioithieu.php
+++ quantri/gioithieu.php
@@ -72,14 +72,7 @@
             <div class="panel-heading">Giới thiệu</div>
             <div class="panel-body">
                 <div class="canvas-wrapper">
-                    <p>Học viện Công nghệ Vietpro được thành lập vào ngày 06/05/2009, hoạt động trong lĩnh vực đào tạo công nghệ thông tin, đặc biệt là đào tạo chuyên sâu trong lĩnh vực lập trình và đồ họa website với trụ sở chính đặt tại Hà Nội<br/>
-                        - Cơ sở 1: Số nhà B8A Ngõ 18 Võ Văn Dũng, Hoàng Cầu, Đống Đa, Hà Nội (Đầu Ngõ 18 đi vào 10m rẽ phải 5m)<br/>
-                        - Cơ sở 2: Số 25 Ngõ 178/71 Tây Sơn, Đống Đa, Hà Nội. (Ngõ 178 đối diện với Trường ĐH Công Đoàn)</p>
-
-
-
-                    <br/>
-                    <p>Đây là hệ thống quản trị của website Thương mại điện tử do Học viện Công nghệ Vietpro xây dựng và phát triển, dành cho học viên của khóa học Lập trình web PHP PRO (Nghiêm cấm sao lưu hay chia sẻ dưới mọi hình thức đối với những ai không phải là học viên của Vietpro)</p>
+                    <p>Đây là hệ thống quản trị của website Thương mại điện tử do nhóm ABC</p>
                     <br/>
                     <p>Hệ thống quản trị này có các chức năng quản lý sau: <br/>
                         - Quản lý Thành viên 
@@ -90,10 +83,8 @@
                         <br/>
                         - ...</p>
                     <br/>
-                    <p>Hệ thống đang trong quá trình hoàn thiện bởi các Chuyên gia Công nghệ web của Học viện Công nghệ Vietpro và các bạn học viên. Hệ thống vẫn tiếp tục được nâng cấp và cải tiến để cho các bạn học viên các khóa học sau được sử dụng những chức năng tốt nhất của hệ thống</p>
-                    <br/>
-                    <p>
-                        <b>Vietpro Academy!</b></p>
+                   
+                    
                 </div>
             </div>
         </div>
